a202295523cb01821944535bd60b5ffdddde6a5a
Utsob1621 <utsobsaha9072@gmail.com>
2026-03-21 18:35:02 +0600

    implement dynamic pet selection and model-sub-container update

--- script/index.js
+++ script/index.js
@@ -13,7 +13,7 @@ const displayCategory = (categories) => {
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
       <button onclick = loadPets('${cat.category}') class = "btn bg-[#FFF0F5] hover:bg-white font-medium text-xl">${cat.category} 
-      <img class = "w-6 lg:w-8" src="${cat.category_icon}" alt=""></button>
+      <img class = "w-6 lg:w-8 animate-pulse" src="${cat.category_icon}" alt=""></button>
     `;
     categoryContainer.appendChild(categoryDiv);
   });
@@ -22,21 +22,21 @@ const displayCategory = (categories) => {
 // displayCategory section
 
 const loadPets = async (categoryName) => {
-  spinnerShow("loading-data")
-  foundNoteHide("no-data" );
+  spinnerShow("loading-data");
+  foundNoteHide("no-data");
   const response = await fetch(
     `https://openapi.programming-hero.com/api/peddy/category/${categoryName}`,
   );
   const data = await response.json();
-  if(data.data){
+  if (data.data) {
     displayPets(data.data);
-    spinnerHide("loading-data")
+    spinnerHide("loading-data");
   }
 };
 
 const displayPets = (pets) => {
   if (pets.length <= 0) {
-    foundNoteShow("no-data" );
+    foundNoteShow("no-data");
   }
   const petsContainer = document.getElementById("pets-container");
   petsContainer.innerHTML = "";
@@ -50,44 +50,61 @@ const displayPets = (pets) => {
       alt="Pet's" />
   </figure>
   <div class="card-body">
-    <div class=" flex justify-around gap-8 mb-2">
+    <div class=" flex justify-around gap-8">
     <h2 class="card-title font-medium">Name : ${pet.pet_name}</h2>
-    <h2 class="card-title font-medium">Gender : ${pet.gender ? pet.gender:" "}</h2>
+    <h2 class="card-gender font-medium">Gender : ${pet.gender ? pet.gender : " "}</h2>
     </div>
     <p class = "line-clamp-2 font-normal">${pet.pet_details}</p>
     <div class="card-actions justify-start">
-      <button class="btn btn-primary text-white font-medium">Select</button>
-      <button class="btn btn-primary text-white font-medium">Details</button>
+      <button onclick="my_modal_1.showModal()" class="btn btn-primary text-white font-medium select w-full">Select</button>
+      <button class="btn btn-primary text-white font-medium w-full">Details</button>
     </div>
   </div>
 </div>
     `;
     petsContainer.appendChild(petsDiv);
   });
+
+  const allSelectButton = document.getElementsByClassName("select");
+  for (const button of allSelectButton) {
+    button.addEventListener("click", (event) => {
+      const cardBody = event.target.closest(".card-body");
+      // const petImage = cardBody.querySelector(".image").innerText;
+      const petTitle = cardBody.querySelector(".card-title").innerText;
+      const gender = cardBody.querySelector(".card-gender").innerText;
+      openModel(petTitle, gender);
+      console.log(cardBody);
+    });
+  }
 };
 
-
-
-
-
-
+const openModel = (title, gen) => {
+  const modelSubContainer = document.getElementById("model-sub-container");
+  const modelDiv = document.createElement("div");
+  modelDiv.classList.add("border", "border-[#FFF0F5]", "p-5", "shadow-sm", "rounded-lg", "mb-5")
+  modelDiv.innerHTML = `
+      <h2 class="card-title font-medium">Name : ${title? title : " "}</h2>
+      <h2 class="card-gender font-medium">Gender : ${gen}</h2>
+      
+    `;
+  modelSubContainer.appendChild(modelDiv);
+};
 
 const foundNoteHide = (id) => {
   document.getElementById(id).style.display = "none";
-}
+};
 
 const foundNoteShow = (id) => {
-   document.getElementById(id).style.display = "block";
-}
+  document.getElementById(id).style.display = "block";
+};
 
 const spinnerShow = (id) => {
   document.getElementById(id).style.display = "block";
-}
+};
 
 const spinnerHide = (id) => {
-   document.getElementById(id).style.display = "none";
-}
-
+  document.getElementById(id).style.display = "none";
+};
 
 loadCategory();
 loadPets("cat");
